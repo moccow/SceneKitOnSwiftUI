2f20f1026705a55d697d4a79ec27f96c8e0839d7
moccow <moccow@gmail.com>
2020-12-29 13:31:26 +0900

    Initial Commit

---
+++ SceneKitOnSwiftUI/SceneKitOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneKitOnSwiftUIApp.swift
+//  SceneKitOnSwiftUI
+//
+//  Created by masami ishiyama on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct SceneKitOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
